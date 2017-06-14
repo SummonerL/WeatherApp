07e7a6a3b53a82902d5d799d7b1a4bbe7ee84aee
Elliot Simpson <Elliotsminion@gmail.com>
2017-06-14 00:00:23 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   request('http://api.openweathermap.org/data/2.5/weather?zip=28403,us&units=imperial&appid=652444958803da77800a6313546a40db', function(err, response, body){
     if (err) throw err;
     console.log(body);
-    res.render('index', { title: 'Wilmington, NC', weatherData: JSON.parse(body) });
+    res.render('index', { title: 'Wilmington, NC', weatherData: JSON.parse(body), time: new Date().toISOString().slice(0, 10) });
   });
 });
 
